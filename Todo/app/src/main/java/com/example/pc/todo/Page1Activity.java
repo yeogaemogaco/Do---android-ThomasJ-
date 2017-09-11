c885212715967f443159c20be5b7d51475e60d12
Ola <ruddmsdl000@naver.com>
2017-09-11 23:40:39 +0900

    Update Page1Activity.java

--- Todo/app/src/main/java/com/example/pc/todo/Page1Activity.java
+++ Todo/app/src/main/java/com/example/pc/todo/Page1Activity.java
@@ -1,12 +1,5 @@
 package com.example.pc.todo;
 
-<<<<<<< HEAD
-/**
- * Created by ola on 08/09/2017.
- */
-
-public class Page1Activity {
-=======
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,5 +10,4 @@ public class Page1Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
     }
->>>>>>> 106b6402e63cd49df63f140e27f85e0e9cefac93
 }
